e184c2c41358abd31687cff74d5e8719b1e507fc
Mohammad Wasil <mwasil.wasil@smail.inf.h-brs.de>
2017-12-15 11:45:09 +0100

    indentation

--- ig_active_reconstruction/src/code_base/weighted_linear_utility.cpp
+++ ig_active_reconstruction/src/code_base/weighted_linear_utility.cpp
@@ -84,30 +84,30 @@ namespace ig_active_reconstruction
       // cost
       if( robot_comm_unit_!=nullptr && cost_weight_!=0 )
       {
-	cost = robot_comm_unit_->movementCost(view);
+	      cost = robot_comm_unit_->movementCost(view);
 	
-	if( cost.exception != robot::MovementCost::Exception::NONE )
-	  continue; // invalid view... disregard in calculation
-	else
-	  cost_val = cost.cost;
+        if( cost.exception != robot::MovementCost::Exception::NONE )
+          continue; // invalid view... disregard in calculation
+        else
+          cost_val = cost.cost;
       }
       
       // information gain - non multithreaded version
       /*if( world_comm_unit_!=nullptr )
       {
-	command.path.clear();
-	command.path.push_back( view.pose() );
-	
-	world_comm_unit_->computeViewIg(command,information_gains);
-	for( unsigned int i= 0; i<information_gains.size(); ++i )
-	{
-	  if( information_gains[i].status == world_representation::CommunicationInterface::ResultInformation::SUCCEEDED )
-	  {
-	    //std::cout<<"\nReturned gain of metric "<<i<<":"<<information_gains[i].predicted_gain;
-	    ig_val += ig_weights_[i]*information_gains[i].predicted_gain;
-	  }
-	}
-	//std::cout<<"\nReturned total information gain is: "<<ig_val;
+        command.path.clear();
+        command.path.push_back( view.pose() );
+        
+        world_comm_unit_->computeViewIg(command,information_gains);
+        for( unsigned int i= 0; i<information_gains.size(); ++i )
+        {
+          if( information_gains[i].status == world_representation::CommunicationInterface::ResultInformation::SUCCEEDED )
+          {
+            //std::cout<<"\nReturned gain of metric "<<i<<":"<<information_gains[i].predicted_gain;
+            ig_val += ig_weights_[i]*information_gains[i].predicted_gain;
+          }
+        }
+        //std::cout<<"\nReturned total information gain is: "<<ig_val;
       }      
       total_ig += ig_val;
       ig_vector.push_back(ig_val);
@@ -151,42 +151,44 @@ namespace ig_active_reconstruction
       std::cout<<"\nutility of view "<<id_set[i]<<": "<<utility;
       if( utility>best_util )
       {
-	best_util = utility;
-	nbv = id_set[i];
+        best_util = utility;
+        nbv = id_set[i];
       }
     }
     //std::cout<<"\nChoosing view "<<nbv<<".";
     return nbv;
   }
   
-  void WeightedLinearUtility::getIg(std::vector<double>& ig_vector,double& total_ig, world_representation::CommunicationInterface::IgRetrievalCommand command, views::ViewSpace::IdSet& id_set, boost::shared_ptr<views::ViewSpace> viewspace, unsigned int base_index, unsigned int batch_size )
+  void WeightedLinearUtility::getIg(std::vector<double>& ig_vector,double& total_ig, 
+                                    world_representation::CommunicationInterface::IgRetrievalCommand command, 
+                                    views::ViewSpace::IdSet& id_set, boost::shared_ptr<views::ViewSpace> viewspace, 
+                                    unsigned int base_index, unsigned int batch_size )
   {
-    
     // information gain
     if( world_comm_unit_!=nullptr )
     {
       for( size_t i = base_index; i<id_set.size(); i+=batch_size )
       {	
-	views::View view = viewspace->getView( id_set[i] );
-	
-	world_representation::CommunicationInterface::ViewIgResult information_gains;
-	double ig_val = 0;
-	
-	command.path.clear();
-	command.path.push_back( view.pose() );
-	
-	world_comm_unit_->computeViewIg(command,information_gains);
-	
-	for( unsigned int i= 0; i<information_gains.size(); ++i )
-	{
-	  if( information_gains[i].status == world_representation::CommunicationInterface::ResultInformation::SUCCEEDED )
-	  {
-	    //std::cout<<"\nReturned gain of metric "<<i<<":"<<information_gains[i].predicted_gain;
-	    ig_val += ig_weights_[i]*information_gains[i].predicted_gain;
-	  }
-	}
-	total_ig += ig_val;
-	ig_vector[i] = ig_val;
+        views::View view = viewspace->getView( id_set[i] );
+        
+        world_representation::CommunicationInterface::ViewIgResult information_gains;
+        double ig_val = 0;
+        
+        command.path.clear();
+        command.path.push_back( view.pose() );
+        
+        world_comm_unit_->computeViewIg(command,information_gains);
+        
+        for( unsigned int i= 0; i<information_gains.size(); ++i )
+        {
+          if( information_gains[i].status == world_representation::CommunicationInterface::ResultInformation::SUCCEEDED )
+          {
+            //std::cout<<"\nReturned gain of metric "<<i<<":"<<information_gains[i].predicted_gain;
+            ig_val += ig_weights_[i]*information_gains[i].predicted_gain;
+          }
+        }
+        total_ig += ig_val;
+        ig_vector[i] = ig_val;
       }
       return;
     }
