267d792e3aaa51c0ea3585050338dfd0d7c6e4e2
mohammad <mwasil.wasil@smail.inf.h-brs.de>
2018-01-05 22:16:09 +0100

    fix service response

--- ig_active_reconstruction_youbot/src/nodes/yb_perceive_location_client_node.py
+++ ig_active_reconstruction_youbot/src/nodes/yb_perceive_location_client_node.py
@@ -8,8 +8,8 @@ from mir_yb_action_msgs.msg import PerceiveLocationAction, PerceiveLocationGoal
 from ig_active_reconstruction_msgs.srv import youbotObjectDetector
 
 
-def detect_object(data):  
-    rospy.loginfo("Object detection is requested with obj numb: %s", data.obj_number)
+def detect_object(req):  
+    rospy.loginfo("Object detection is requested with obj numb: %s", req.obj_number)
     client = actionlib.SimpleActionClient('perceive_location_server', PerceiveLocationAction)
     client.wait_for_server()
     goal = PerceiveLocationGoal()
@@ -19,8 +19,14 @@ def detect_object(data):
     rospy.loginfo("Result ")
     print client.get_result()
     result = len(client.get_result().object_list)
+    print result
     #publish_obj_list(result)
-    return youbotObjectDetectorResponse(result)
+    #rsp = youbotObjectDetector()
+    #req.result = result
+    rsp = Int32()
+    rsp.data = result
+    youbotObjectDetector.result = rsp
+    return youbotObjectDetector.result
 
 def object_detector_server():
     rospy.init_node('yb_perceive_location_client_node')
